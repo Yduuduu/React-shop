3b93360cffe59a608cd6a99b9a0986b72408d311
Yduuduu <hou_o@naver.com>
2022-05-06 23:02:55 +0900

    shop

--- client/src/components/views/NavBar/NavBar.js
+++ client/src/components/views/NavBar/NavBar.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import LeftMenu from './Sections/LeftMenu';
 import RightMenu from './Sections/RightMenu';
-import { Drawer, Button, Icon } from 'antd';
+import { Drawer, Button } from 'antd';
 import './Sections/Navbar.css';
+import Icon from '@ant-design/icons';
 
 function NavBar() {
   const [visible, setVisible] = useState(false)
